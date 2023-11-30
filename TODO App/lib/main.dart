9a2e33509e2b955999e10f458c9b3de2b06adf4c
Waleed-Elgendy <waleedelgendy3@gmail.com>
2023-11-30 20:40:04 +0200

    add TODO App project and Readme file

---
+++ TODO App/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:todo/pages/home_page.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+        designSize: const Size(411.42857142857144, 843.4285714285714),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) {
+          return const MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: HomePage(),
+          );
+        }
+    );
+  }
+
+}
+
+
