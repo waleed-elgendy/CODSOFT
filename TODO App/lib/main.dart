384de873730f6c13395160d01a46b21b739e4e82
Waleed-Elgendy <waleedelgendy3@gmail.com>
2024-01-09 12:26:28 +0200

    .

--- TODO App/lib/main.dart
+++ TODO App/lib/main.dart
@@ -1,13 +1,10 @@
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:todo/cubits/view_tasks_cubit/view_tasks_cubit.dart';
 import 'package:todo/firebase_options.dart';
 import 'package:todo/helper/auth_widget.dart';
 import 'package:todo/simple_bloc_observer.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,26 +23,19 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
-  bool isLogIn=false;
+  bool isLogIn = false;
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     return ScreenUtilInit(
         designSize: const Size(411.42857142857144, 843.4285714285714),
         minTextAdapt: true,
         splitScreenMode: true,
         builder: (context, child) {
-
-          return BlocProvider(
-            create: (context) => ViewTasksCubit(),
-            child:  const MaterialApp(
-              debugShowCheckedModeBanner: false,
-
-              home:AuthWidget(),
-            ),
+          return const MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: AuthWidget(),
           );
         });
   }
