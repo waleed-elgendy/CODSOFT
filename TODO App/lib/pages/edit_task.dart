79a63ed2ae98b905df0ea3fb211c8214babceee7
Waleed-Elgendy <waleedelgendy3@gmail.com>
2024-01-09 12:32:16 +0200

    refactor some code

--- TODO App/lib/pages/edit_task.dart
+++ TODO App/lib/pages/edit_task.dart
@@ -16,11 +16,13 @@ import 'package:todo/sharedWidgets/custom_button.dart';
 import 'package:todo/sharedWidgets/custom_text.dart';
 import 'package:todo/sharedWidgets/custom_text_field.dart';
 import 'package:todo/sharedWidgets/dateTime_widget.dart';
+import 'package:todo/sharedWidgets/edit_task_icons.dart';
 import 'package:todo/sharedWidgets/first_two_positioned.dart';
 import 'package:todo/sharedWidgets/icon_container.dart';
 
 class EditTask extends StatefulWidget {
-   EditTask({Key? key, required this.user,required this.task}) : super(key: key);
+  EditTask({Key? key, required this.user, required this.task})
+      : super(key: key);
 
   String user;
   TaskModel task;
@@ -29,7 +31,7 @@ class EditTask extends StatefulWidget {
 }
 
 class _EditTaskState extends State<EditTask> {
- late String taskTitle;
+  late String taskTitle;
   String? title = '';
   bool isComplete = false;
   TextEditingController dateController = TextEditingController();
@@ -43,30 +45,34 @@ class _EditTaskState extends State<EditTask> {
   bool isLoading = false;
   @override
   void initState() {
-    taskTitle=widget.task.taskTitle;
-    taskController.text=widget.task.taskTitle;
-    dateController.text = widget.task.date == null? "": widget.task.date.toString();
-    timeController.text= widget.task.time == null? "": widget.task.time.toString();
-    notesController.text=widget.task.notes == null? "": widget.task.notes.toString();
-    title=widget.task.taskTitle;
+    taskTitle = widget.task.taskTitle;
+    taskController.text = widget.task.taskTitle;
+    dateController.text =
+        widget.task.date == null ? "" : widget.task.date.toString();
+    timeController.text =
+        widget.task.time == null ? "" : widget.task.time.toString();
+    notesController.text =
+        widget.task.notes == null ? "" : widget.task.notes.toString();
+    title = widget.task.taskTitle;
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
-
     CollectionReference deleteTasks =
-    FirebaseFirestore.instance.collection(widget.user);
+        FirebaseFirestore.instance.collection(widget.user);
     return BlocProvider(
       create: (context) => AddTaskCubit(),
       child:
-      BlocConsumer<AddTaskCubit, AddTaskStates>(listener: (context, state) {
+          BlocConsumer<AddTaskCubit, AddTaskStates>(listener: (context, state) {
         if (state is AddTaskLoading) {
           isLoading = true;
           setState(() {});
         }
         if (state is AddTaskFailure) {
           isLoading = false;
-          showSnackBar(context, 'try again',"Edit failed",ContentType.failure);
+          showSnackBar(
+              context, 'try again', "Edit failed", ContentType.failure);
         }
         if (state is AddTaskSuccess) {
           isLoading = false;
@@ -80,10 +86,10 @@ class _EditTaskState extends State<EditTask> {
             key: formKey,
             child: Scaffold(
               extendBodyBehindAppBar: true,
-              appBar:CustomAppBar(
+              appBar: CustomAppBar(
                 height: 40.h,
                 leading: GestureDetector(
-                  onTap: (){
+                  onTap: () {
                     Navigator.pop(context);
                   },
                   child: const IconContainer(
@@ -103,11 +109,17 @@ class _EditTaskState extends State<EditTask> {
                     child: Stack(
                       children: [
                         const FirstPositioned(),
-                         MainColorContainer(height: 100.h),
+                        MainColorContainer(height: 100.h),
                         PositionedCircle(
-                            left: -140.w, top: -45.h, width: 280.w, height: 300.h),
+                            left: -140.w,
+                            top: -45.h,
+                            width: 280.w,
+                            height: 300.h),
                         PositionedCircle(
-                            right: -100.w, top: -45.h, width: 180.w, height: 180.h),
+                            right: -100.w,
+                            top: -45.h,
+                            width: 180.w,
+                            height: 180.h),
                         Positioned(
                           top: 120.h,
                           left: 20.w,
@@ -134,7 +146,7 @@ class _EditTaskState extends State<EditTask> {
                                   fill: true,
                                   textColor: Colors.black,
                                   onchange: (data) {
-                                    widget.task.taskTitle=data!;
+                                    widget.task.taskTitle = data!;
                                     title = data;
                                     setState(() {});
                                   },
@@ -153,21 +165,21 @@ class _EditTaskState extends State<EditTask> {
                                     controller: dateController,
                                     onTap: () async {
                                       var datePicked =
-                                      await DatePicker.showSimpleDatePicker(
+                                          await DatePicker.showSimpleDatePicker(
                                         backgroundColor: Colors.white,
                                         context,
-                                        firstDate: DateTime(DateTime.now().year),
+                                        firstDate:
+                                            DateTime(DateTime.now().year),
                                         lastDate: DateTime(2090),
                                         dateFormat: "dd-MMMM-yyyy",
                                         locale: DateTimePickerLocale.en_us,
                                         looping: true,
                                       );
                                       widget.task.date =
-                                      "${datePicked.toString().substring(8, 10)}-${datePicked.toString().substring(5, 7)}";
+                                          "${datePicked.toString().substring(8, 10)}-${datePicked.toString().substring(5, 7)}";
                                       if (datePicked != null) {
                                         dateController.text =
-                                        "${datePicked.toString().substring(8, 10)}-${datePicked.toString().substring(5, 7)}";
-
+                                            "${datePicked.toString().substring(8, 10)}-${datePicked.toString().substring(5, 7)}";
                                       }
                                     },
                                     hint: 'Date',
@@ -180,16 +192,19 @@ class _EditTaskState extends State<EditTask> {
                                   DateOrTimeWidget(
                                     controller: timeController,
                                     onTap: () async {
-                                      final TimeOfDay? newTime = await showTimePicker(
+                                      final TimeOfDay? newTime =
+                                          await showTimePicker(
                                         context: context,
                                         initialTime: TimeOfDay(
                                             hour: DateTime.now().hour,
                                             minute: DateTime.now().minute),
                                       );
-                                      widget.task.time = newTime.toString().substring(10, 15);
+                                      widget.task.time =
+                                          newTime.toString().substring(10, 15);
                                       if (newTime != null) {
-                                        timeController.text =
-                                            newTime.toString().substring(10, 15);
+                                        timeController.text = newTime
+                                            .toString()
+                                            .substring(10, 15);
                                       }
                                     },
                                     hint: 'Time',
@@ -216,43 +231,50 @@ class _EditTaskState extends State<EditTask> {
                                   textColor: Colors.black,
                                   onchange: (data) {
                                     widget.task.notes = data;
-                                    notesController.text=data!;
+                                    notesController.text = data!;
                                   },
                                 ),
                               ),
                               SizedBox(
-                                height: 61.h,
+                                height: 38.h,
                               ),
                               title != ''
                                   ? CustomButton(
-                                  color: const Color(0xff4A3780),
-                                  text: 'Save',
-                                  onTap: () async{
-                                    print(taskTitle);
-                                    if (formKey.currentState!.validate()) {
-                                      formKey.currentState!.save();
-                                      var task = TaskModel(
-                                          taskTitle: widget.task.taskTitle,
-                                          iconCodePoint: widget.task.iconCodePoint,
-                                          iconColor: widget.task.iconColor,
-                                          conColor: widget.task.conColor,
-                                          notes: widget.task.notes,
-                                          time: widget.task.time,
-                                          date: widget.task.date,
-                                          isComplete: widget.task.isComplete
-
-                                          );
-                                      BlocProvider.of<AddTaskCubit>(context)
-                                          .addTask(task, widget.task.taskTitle==taskTitle?taskTitle:widget.task.taskTitle, widget.user);
-                                      if(widget.task.taskTitle!=taskTitle){
-                                        await deleteTasks
-                                            .doc(taskTitle)
-                                            .delete();
-                                      }
-                                    } else {
-                                      autoValidateMode = AutovalidateMode.always;
-                                    }
-                                  })
+                                      color: const Color(0xff4A3780),
+                                      text: 'Save',
+                                      onTap: () async {
+                                        if (formKey.currentState!.validate()) {
+                                          formKey.currentState!.save();
+                                          var task = TaskModel(
+                                              taskTitle: widget.task.taskTitle,
+                                              iconCodePoint:
+                                                  widget.task.iconCodePoint,
+                                              iconColor: widget.task.iconColor,
+                                              conColor: widget.task.conColor,
+                                              notes: widget.task.notes,
+                                              time: widget.task.time,
+                                              date: widget.task.date,
+                                              isComplete:
+                                                  widget.task.isComplete);
+                                          BlocProvider.of<AddTaskCubit>(context)
+                                              .addTask(
+                                                  task,
+                                                  widget.task.taskTitle ==
+                                                          taskTitle
+                                                      ? taskTitle
+                                                      : widget.task.taskTitle,
+                                                  widget.user);
+                                          if (widget.task.taskTitle !=
+                                              taskTitle) {
+                                            await deleteTasks
+                                                .doc(taskTitle)
+                                                .delete();
+                                          }
+                                        } else {
+                                          autoValidateMode =
+                                              AutovalidateMode.always;
+                                        }
+                                      })
                                   : const SizedBox(),
                             ],
                           ),
@@ -269,85 +291,3 @@ class _EditTaskState extends State<EditTask> {
     );
   }
 }
-class EditTaskIcon extends StatefulWidget {
-  const EditTaskIcon({Key? key, required this.task}) : super(key: key);
-
-  final TaskModel task;
-  @override
-  State<EditTaskIcon> createState() => _EditTaskIconState();
-}
-
-class _EditTaskIconState extends State<EditTaskIcon> {
-   late int currentIndex ;
-
-    List<Color> contColors = const [
-      Color(0xffDBECF6),
-      Color(0xffE7E2F3),
-      Color(0xffFEF5D3),
-    ];
-    List<Color> iconColors = const [
-      Color(0xff194A66),
-      Color(0xff4A3780),
-      Color(0xff403100),
-    ];
-    List<IconData> icons = const [
-      Icons.list_alt,
-      Icons.event,
-      Icons.emoji_events_outlined,
-    ];
-
-    @override
-  void initState() {
-    currentIndex=iconColors.indexOf(Color(widget.task.iconColor));
-    super.initState();
-  }
-    @override
-    Widget build(BuildContext context) {
-      return Row(
-        children: [
-          Text(
-            "Category",
-            style: TextStyle(
-              fontSize: 18.sp,
-              color: Colors.black,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-          SizedBox(
-            width: 25.w,
-          ),
-          SizedBox(
-            width: 200.w,
-            height: 60.h,
-            child: ListView.builder(
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: iconColors.length,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 4.w),
-                    child: GestureDetector(
-                      onTap: () {
-                        currentIndex = index;
-                        BlocProvider.of<AddTaskCubit>(context).iconCodePoint =
-                            icons[index].codePoint;
-                        BlocProvider.of<AddTaskCubit>(context).iconColor =
-                            iconColors[index].value;
-                        BlocProvider.of<AddTaskCubit>(context).conColor =
-                            contColors[index].value;
-                        setState(() {});
-                      },
-                      child: IconContainer(
-                        contColor: contColors[index],
-                        icon: icons[index],
-                        iconColor: iconColors[index],
-                        isSelected: currentIndex == index,
-                      ),
-                    ),
-                  );
-                }),
-          ),
-        ],
-      );
-    }
-  }
